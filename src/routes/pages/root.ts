65a1e79be077a1b5921f97a334137259736b85d6
Sanni Michael <sannimichaelse@gmail.com>
2021-11-15 22:39:28 +0100

    updated landing route

--- src/routes/pages/root.ts
+++ src/routes/pages/root.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).header('Content-Type', 'text/html').send(`<h4>💊 Jetti.io Take Home Assignment</h4>`);
+  res.status(200).header('Content-Type', 'text/html').send(`<h4>💊 <a href="https://jetti.io/">Jetti.io</a> Take Home Assignment</h4>`);
 });
 
 export default router;
