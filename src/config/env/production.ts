1fc1ed2cb2b6d51407ffde03a6e84148d3bf762b
Sanni Michael <sannimichaelse@gmail.com>
2021-11-15 20:51:23 +0100

    added github actions

--- src/config/env/production.ts
+++ src/config/env/production.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const production = {
-    APP_PORT: process.env.PORT,
+    APP_PORT: process.env.JETTI_APP_PORT,
     JETTI_VENDOR_SUBMISSION_URL: process.env.JETTI_VENDOR_SUBMISSION_URL,
     JETTI_CUSTOMER_UUID_URL: process.env.JETTI_CUSTOMER_UUID_URL,
     JETTI_SALES_URL: process.env.JETTI_SALES_URL,
